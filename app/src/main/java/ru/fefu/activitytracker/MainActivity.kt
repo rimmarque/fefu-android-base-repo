c707d19719b8b41972c83499a262bd4dfbad973c
rimmarque <nikolaeva.ro@students.dvfu.ru>
2022-03-22 21:23:20 +1000

    homework 2

---
+++ app/src/main/java/ru/fefu/activitytracker/MainActivity.kt
@@ -0,0 +1,60 @@
+package ru.fefu.activitytracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.fefu.activitytracker.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction().apply {
+                add(
+                    R.id.fragmentContainerView,
+                    ActivityFragmentFlow(),
+                    "activityFragment"
+                )
+                commit()
+            }
+        }
+
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            val activityFragment = supportFragmentManager.findFragmentByTag("activityFragment")
+            val profileFragment = supportFragmentManager.findFragmentByTag("profileFragment")
+            when (it.itemId) {
+                R.id.navigation_activity -> {
+                    if (profileFragment !== null) {
+                        supportFragmentManager.beginTransaction().hide(profileFragment).commit()
+                    }
+                    if (activityFragment !== null) {
+                        supportFragmentManager.beginTransaction().show(activityFragment).commit()
+                    }
+                }
+
+                R.id.navigation_profile -> {
+                    if (activityFragment !== null) {
+                        supportFragmentManager.beginTransaction().hide(activityFragment).commit()
+                    }
+                    if (profileFragment !== null) {
+                        supportFragmentManager.beginTransaction().show(profileFragment).commit()
+                    } else {
+                        supportFragmentManager.beginTransaction().apply {
+                            add(
+                                R.id.fragmentContainerView,
+                                ProfileFragment(),
+                                "profileFragment"
+                            )
+                            commit()
+                        }
+                    }
+                }
+            }
+            true
+        }
+    }
+}
---
+++ app/src/main/java/ru/fefu/activitytracker/UsersFragmentActivity.kt
@@ -0,0 +1,24 @@
+package ru.fefu.activitytracker.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.fefu.activitytracker.R
+
+class UsersFragmentActivity : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_users_activity, container, false)
+    }
+
+}
